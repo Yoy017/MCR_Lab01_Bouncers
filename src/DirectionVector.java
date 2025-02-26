912072d5310b96de583ab75086c1e02ece3495e1
RemyBlr <remy.bleuer@hotmail.com>
2025-02-26 14:01:41 +0100

    Added vector

---
+++ src/DirectionVector.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class DirectionVector {
+    public double dx, dy;
+    private static final Random random = new Random();
+
+    public DirectionVector(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public static DirectionVector randomDirection(double minSpeed, double maxSpeed) {
+        /*// random speed between minSpeed and maxSpeed
+        double speed = minSpeed + (random.nextDouble() * (maxSpeed - minSpeed));
+        // random angle between 0 and 2PI
+        //double angle = random.nextDouble() * 2 * Math.PI;
+        double angle;
+        do {
+            angle = random.nextDouble() * 2 * Math.PI;
+        } while(Math.abs(Math.cos(angle)) < 0.5 || Math.abs(Math.sin(angle)) < 0.5);
+
+        return new DirectionVector(speed * Math.cos(angle), speed * Math.sin(angle));*/
+        double speedX = (random.nextDouble() * (maxSpeed - minSpeed)) + minSpeed;
+        double speedY = (random.nextDouble() * (maxSpeed - minSpeed)) + minSpeed;
+
+        if (random.nextBoolean()) speedX = -speedX;
+        if (random.nextBoolean()) speedY = -speedY;
+
+        return new DirectionVector(speedX, speedY);
+    }
+
+    public void invertX() {dx = -dx;}
+
+    public void invertY() {dy = -dy;}
+}
--- src/Entity.java
+++ src/Entity.java
@@ -1,40 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 abstract public class Entity extends JComponent {
     protected Color color;
-    protected int size, x, y, dx, dy;
-    private static final Random random = new Random();
+    protected int size, x, y;
+    protected DirectionVector movement;
 
     protected Entity(Color color) {
         this.color = color;
-        this.size = random.nextInt(10) + 10; // random size between 10 and 20 px
+        this.size = (int) (Math.random() * 5) + 15; // random size between 5 and 20 px
         // initial position
         this.x = 0;
         this.y = 0;
-        // random speed but not working for now
-        // TODO
-        /*do {
-            this.dx = random.nextInt(1) + 4; // random speed between 2 and 4
-            this.dy = random.nextInt(1) + 4; // random speed between 2 and 4
-        } while (dx == 0 && dy == 0);
-        // random direction
-        if (random.nextBoolean()) dx = -dx;
-        if (random.nextBoolean()) dy = -dy;*/
-
-        dx = (random.nextInt(5) + 1) * (random.nextBoolean() ? 1 : -1);
-        dy = (random.nextInt(5) + 1) * (random.nextBoolean() ? 1 : -1);
+        // vector of movement
+        this.movement = DirectionVector.randomDirection(1, 5);
     }
 
     public void move(JPanel panel) {
         int panelWidth = panel.getWidth();
         int panelHeight = panel.getHeight();
 
-        y += dy;
-        x += dx;
-        if(y <= 0 || y + size >= panelHeight) dy = -dy;
-        if(x <= 0 || x + size >= panelWidth) dx = -dx;
+        y += (int) movement.dy;
+        x += (int) movement.dx;
+        if(y <= 0 || y + size >= panelHeight) movement.invertY();
+        if(x <= 0 || x + size >= panelWidth) movement.invertX();
     }
 
     public abstract void draw(Graphics g);
